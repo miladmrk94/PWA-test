9c5f60f0b3b656c01093f134e2ab109b4de4f501
milad <milad.rastgoou@gmail.com>
2023-05-02 12:29:07 +0330

    [test] PWA

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = `food-recipes`;
+
+// Use the install event to pre-cache all initial resources.
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll([
+            '/',
+            '/main.js',
+            '/style.css',
+            '/images/chef-favicon.png',
+            '/images/chef-icon.png',
+            '/images/curry.png',
+            '/images/noodles.png',
+            '/images/stew.png',
+        ]);
+    })());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith((async () => {
+        const cache = await caches.open(CACHE_NAME);
+
+        // Get the resource from the cache.
+        const cachedResponse = await cache.match(event.request);
+        if (cachedResponse) {
+            return cachedResponse;
+        } else {
+            try {
+                // If the resource was not in the cache, try the network.
+                const fetchResponse = await fetch(event.request);
+
+                // Save the resource in the cache and return it.
+                cache.put(event.request, fetchResponse.clone());
+                return fetchResponse;
+            } catch (e) {
+                // The network failed.
+            }
+        }
+    })());
+});
